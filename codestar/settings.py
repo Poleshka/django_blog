2c0f10bf23c9cb04063bc5046104c7f8c28c880c
Paulina Adams <pstafska@gmail.com>
2024-11-15 15:06:25 +0000

    Superuser nr3

--- codestar/settings.py
+++ codestar/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-if os.path.isfile("env.py"):
+
+if os.path.exists("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,19 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "PJ9KJAvj7U")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 
 ALLOWED_HOSTS = [
-        '8000-poleshka-djangoblog-y94a0skcqy0.ws.codeinstitute-ide.net',
-        'django-world1-516fbdd073be.herokuapp.com',
-        'https://*.codeinstitute-ide.net/',
+        '.codeinstitute-ide.net',
         '.herokuapp.com', 
-        'localhost',
-        '127.0.0.1'
         ]
 
 
@@ -93,21 +90,10 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 #     }
 # }
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-        }
-    }
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 CSRF_TRUSTED_ORIGINS = [
     'https://*.codeinstitute-ide.net/',
