f1d53ab8e8c11a799db8eb772997ec18bfa17ff9
Paulina Adams <pstafska@gmail.com>
2024-11-21 14:01:09 +0000

    Added/debugged About_me page

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 from pathlib import Path
 import os
 import dj_database_url
@@ -49,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
